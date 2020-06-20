88f6555ef8b87a0bf71869990098508253d118d5
Van-Tien Hoang <van-tien.hoang.1@etsmtl.net>
2020-06-20 11:42:21 -0500

    Add ability to build on iOS (#14)
    - Availability annotation for macOS and iOS

--- Sources/Plotly/IO/Browser.swift
+++ Sources/Plotly/IO/Browser.swift
@@ -5,21 +5,26 @@ import Foundation
 struct Browser {
     
     /// Opens the specified URL in the default browser available on your OS.
+    #if os(OSX)
     static func open(url: URL) throws {
         let env = URL(fileURLWithPath: "/usr/bin/env")
         let browser = try Process.run(env, arguments: ["open", url.absoluteString])
         browser.waitUntilExit()
     }
-
+    #endif
     /// Exports the `Figure` to a temporary HTML file and displays it using the default browser available on your OS.
+    
+    @available(iOS 10.0, *)
     static func show(figure: Figure, javaScript bundle: HTML.JavaScriptBundleOption) throws {
         let htmlDocument = try HTML.create(from: figure, plotly: bundle, mathJax: bundle, document: true)
 
         let tempDirectory = FileManager().temporaryDirectory
         let tempHtmlFile = tempDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("html")
         try! htmlDocument.write(to: tempHtmlFile, atomically: true, encoding: .utf8)
-
+        #if os(OSX)
         try Browser.open(url: tempHtmlFile)
+        #endif
     }
+    
 
 }
